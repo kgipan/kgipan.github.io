d4f9eacf4b12a051c64e1f1cf03f9d678115e35b
Karla Pan <89999452+kgipan@users.noreply.github.com>
2021-11-24 11:12:07 -0500

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,9 @@
+function myOverFunction(){
+   const element = document.getElementById('project');
+   element.classList.add('on');
+}
+
+function myOffFunction(){
+    const element = document.getElementById('project');
+    element.classList.remove('on');
+}
