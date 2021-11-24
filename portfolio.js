c31a00f87ea3ddc44335ef089f08f5ee8c97ad73
Karla Pan <89999452+kgipan@users.noreply.github.com>
2021-11-24 11:07:11 -0500

    Delete portfolio.js

--- portfolio.js
+++
@@ -1,9 +0,0 @@
-function myOverFunction(){
-   const element = document.getElementById('project');
-   element.classList.add('on');
-}
-
-function myOffFunction(){
-    const element = document.getElementById('project');
-    element.classList.remove('on');
-}
